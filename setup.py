9e4de9522419e7c376ba9999e71e43cc666cbffc
Simon Birkholz <birkholz@cubert-gmbh.de>
2024-09-10 15:56:46 +0200

    fixing setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ import sys
 import io
 import shutil
 import glob
+import subprocess
 
 from setuptools.command.install import install
 from shutil import rmtree, copy
-from setuptools import setup, find_packages, Command 
+from setuptools import setup, find_packages, Command
 from setuptools.command import develop
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -28,22 +29,24 @@ REQUIREMENTS = {
     ],
 }
 
+
 def get_python_version(sep='.') -> str:
     return f'{sys.version_info.major}{sep}{sys.version_info.minor}'
 
+
 def get_pyil_files():
-    with open(os.path.join(here,f'binary_dir_{platform.python_version()}.stamp')) as f:
+    with open(os.path.join(here, f'binary_dir_{platform.python_version()}.stamp')) as f:
         path = f.read().strip('\n')
         if platform.system() == 'Windows':
             files = ['_cuvis_pyil.pyd', 'cuvis_il.py']
-            for file in files:
-                full_path = os.path.join(path, file)
-                copy(full_path, os.path.join(here, 'cuvis_il'))
-        elif platform.system() == "Linux" and "Ubuntu" in platform.version():
+        elif platform.system() == "Linux":
             files = ['_cuvis_pyil.so', 'cuvis_il.py']
-            for file in files:
-                full_path = os.path.join(path, file)
-                copy(full_path, os.path.join(here, 'cuvis_il'))
+        else:
+            raise ValueError("Unsupported OS")
+        for file in files:
+            full_path = os.path.join(path, file)
+            copy(full_path, os.path.join(here, 'cuvis_il'))
+
 
 lib_dir = ""
 if 'CUVIS' in os.environ:
@@ -54,13 +57,15 @@ else:
         'CUVIS SDK does not seem to exist on this machine! Make sure that the environment variable CUVIS is set.')
 
 # taken from https://github.com/navdeep-G/setup.py/blob/master/setup.py
+
+
 class UploadCommand(Command):
     """Support setup.py upload."""
 
     description = 'Build and publish the package.'
     user_options = [
-         ('username=', None, 'pip Username'),
-          ('password=', None, 'pip Password'),
+        ('username=', None, 'pip Username'),
+        ('password=', None, 'pip Password'),
     ]
 
     @staticmethod
@@ -70,7 +75,7 @@ class UploadCommand(Command):
 
     def initialize_options(self):
         # DEPRECATED - Use PYPI API for authentication instead
-        self.username= '__token__'
+        self.username = '__token__'
         self.password = ''
         pass
 
@@ -91,11 +96,14 @@ class UploadCommand(Command):
         # Operating system dependent
         self.status('Building Source and Wheel (universal) distribution…')
         if platform.system() == 'Windows':
-            os.system(f'python setup.py bdist_wheel --python-tag=py{get_python_version("")} --plat-name=win_amd64')
+            os.system(
+                f'python setup.py bdist_wheel --python-tag=py{get_python_version("")} --plat-name=win_amd64')
             self.status('Uploading the package to PyPI via Twine…')
-            os.system(f'twine upload -p {self.password} -u {self.username} -r testpypi dist/*')
-        elif platform.system() == "Linux" and "Ubuntu" in platform.version():
-            os.system(f'python setup.py bdist_wheel --python-tag=py{get_python_version("")} --plat-name=linux_x86_64')
+            os.system(
+                f'twine upload -p {self.password} -u {self.username} -r testpypi dist/*')
+        elif platform.system() == "Linux":
+            os.system(
+                f'python setup.py bdist_wheel --python-tag=py{get_python_version("")} --plat-name=linux_x86_64')
             # Fix the package to work with manylinux
             whl_file = glob.glob('./dist/*.whl')[0]
             self.status('Repairing build...')
@@ -103,12 +111,23 @@ class UploadCommand(Command):
                 os.mkdir('repaired_dist')
             except:
                 pass
+
+            ubuntu_version = subprocess.check_output(['lsb_release', '-rs'])
+
+            # see https://github.com/mayeut/pep600_compliance?tab=readme-ov-file#distro-compatibility
+            version_lookup = {
+                '20.04': 'manylinux_2_31',
+                '22.04': 'manylinux_2_35'
+            }
+
             #  This creates a build compatible with Ubuntu 20.04
-            os.system('auditwheel repair dist/* -w repaired_dist --plat manylinux_2_31_x86_64')
+            os.system(
+                f'auditwheel repair dist/* -w repaired_dist --plat {version_lookup[ubuntu_version]}_x86_64')
             self.status('Uploading the package to PyPI via Twine…')
             # Make sure .pypirc file is configured
             os.system(f'twine upload -r testpypi repaired_dist/*')
         sys.exit()
+
 
 class PostInstallCommand(install):
     """Custom install command to move .so file after installation."""
@@ -119,14 +138,14 @@ class PostInstallCommand(install):
         # On Windows, no post-install step is necessary
         if platform.system() != "Linux":
             return
-        ### .SO FILE
+        # .SO FILE
         # Path to the .so file in the source directory
         source_path = os.path.join(os.path.dirname(__file__), '_cuvis_pyil.so')
         # Destination directory (where the .so file should be moved)
         destination_dir = os.path.join(self.install_lib, 'cuvis_il')
         # Move the .so file
         shutil.copy(source_path, destination_dir)
-        ### .PY FILE
+        # .PY FILE
         # Path to the .so file in the source directory
         source_path = os.path.join(os.path.dirname(__file__), 'cuvis_il.py')
         # Destination directory (where the .so file should be moved)
@@ -134,6 +153,7 @@ class PostInstallCommand(install):
         # Move the .so file
         shutil.copy(source_path, destination_dir)
         print(f"Moved {source_path} to {destination_dir}")
+
 
 add_il = os.path.join(here, "cuvis_il")
 
@@ -156,10 +176,10 @@ setup(
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type='text/markdown',
-    #setup_requires=REQUIREMENTS['setup'],
+    # setup_requires=REQUIREMENTS['setup'],
     install_requires=REQUIREMENTS['install'],
     include_package_data=True,
-	cmdclass={
+        cmdclass={
         'upload': UploadCommand,
         'install': PostInstallCommand,
     },
