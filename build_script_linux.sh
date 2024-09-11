e24ac0b4b5a7aed1ce356b0e3ac5389409483498
Simon Birkholz <birkholz@cubert-gmbh.de>
2024-09-11 10:09:34 +0200

    added linux build script

---
+++ build_script_linux.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+numpy_version="1.22.0"
+
+# CHANGE ME
+search_path="/usr/bin"
+
+# CHANGE ME
+main_builds_dir="/tmp"
+
+subfolders=$(find "$search_path" -maxdepth 1 -name '*python*')
+
+read -p "pip username:" username 
+read -s -p "pip password:" password  
+
+echo ""
+
+for folder in $subfolders; do
+	relative=$(realpath --relative-to="$search_path" "$folder")
+	if [[ "$relative" != "." ]]; then
+		echo "Building Python binaries with $relative..."
+		
+		case $relative in
+			python3.9 | python3.10)
+			numpy_version="1.22.0"
+			;;
+			python3.11)
+			numpy_version="1.23.0"
+			;;
+			python3.12)
+			numpy_version="1.26.0"
+			;;
+		esac
+		
+		echo -e "\t...setting up python environment"
+		rm -rf "venv_$relative"
+		rm -rf "$main_builds_dir/cuvis_pyil_$relative"
+		$relative -m venv "venv_$relative"
+		source venv_$relative/Scripts/activate
+		pip install wheel --upgrade -qq
+		pip install setuptools -qq
+		pip install twine -qq
+		pip install auditwheel -qq
+		pip install numpy=="$numpy_version" -qq
+		
+		echo -e "\t...executing cmake"
+		python_dir="$(pwd)/venv_$relative"
+		cmake -DCMAKE_BUILD_TYPE=Release -DDOXYGEN_BUILD_DOCUMENTATION=FALSE -DPython_ROOT_DIR="$python_dir" -B "$main_builds_dir\cuvis_pyil_$relative" .
+		cmake --build "$main_builds_dir/cuvis_pyil_$relative" --target cuvis_pyil --config Release
+		
+		echo -e "\t...packing python files"
+		CUVIS_NUMPY_COMPILED=$numpy_version python3 setup.py upload --password=$password --username=$username
+	fi
+done
